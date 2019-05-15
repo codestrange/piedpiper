0b9a5c0f2a80b96c1cf36077b0a1637e43c0668e
Roberto Marti Cedeño <r.marti@estudiantes.matcom.uh.cu>
2019-05-14 20:49:15 -0400

    Añadir .gitignore y funciones básicas de ImageProcessing

---
+++ ImageProcessing/img_processing.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+import os
+
+def Read_Images(photos_path = '../muestras',video_path='../example.mp4'):
+    # Playing video from file:
+    cap = cv2.VideoCapture(video_path)
+
+    try:
+        if not os.path.exists(photos_path):
+            os.makedirs(photos_path)
+    except OSError:
+        print ('Error: Creating directory of data')
+
+    currentFrame = 0
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        
+        try:
+            len(frame)
+        except:
+            break
+
+        # Saves image of the current frame in jpg file
+        name =  photos_path + '/frame' + str(currentFrame) + '.jpg'
+        print ('Creating... ' + name)
+        cv2.imwrite(name, frame)
+
+        # To stop duplicate images
+        currentFrame += 1
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows() 
+
+def Make_Video(images_cnt, photos_path = '../muestras', video_path = '../video/video.avi'):
+    img=[]
+    for i in range(0,images_cnt):
+        img.append(cv2.imread(photos_path + '/frame' + str(i) + '.jpg'))
+
+    height,width,layers=img[1].shape
+
+    #cv2.VideoWriter_fourcc('M','J','P','G')
+    #video=cv2.VideoWriter('./video.avi',-1,1,(width,height))
+    video=cv2.VideoWriter(video_name,cv2.VideoWriter_fourcc(*'DIVX'), 50, (width,height))
+
+
+    for j in range(0,images_cnt):
+        video.write(img[j])
+
+    cv2.destroyAllWindows()
+    video.release() 
